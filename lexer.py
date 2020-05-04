0110a13c59759f2397d7bfeea70c8b21af67c7b7
Евгений Молодан <molodan.e@mail.ru>
2020-05-04 15:59:16 +0700

    lexer v0.1

---
+++ lexer.py
@@ -0,0 +1,160 @@
+import ctypes
+import ply.lex as lex
+
+
+ESCAPE_SEQUENCES = {
+	r'\a': 7,  # bell
+	r'\b': 8,  # backspace
+
+	r'\t': 9,  # h tab
+	r'\n': 10, # new line
+	r'\v': 11, # v tab
+	r'\f': 12, # new page
+	r'\r': 13, # carriage return
+
+	r'\"': 34,
+	r'\'': 39,
+	r'\\': 92
+}
+
+
+class Lexer:
+	keywords = {
+		'if'     :'IF',
+		'elif'   :'ELIF',
+		'else'   :'ELSE',
+		'while'  :'WHILE',
+		'do'     :'DO',
+		'end'    :'END',
+		'proc'   :'PROC',
+		'func'   :'FUNC',
+		'return' :'RETURN',
+		'not'    :'NOT',
+		'and'    :'AND',
+		'or'     :'OR',
+		'var'    :'VAR',
+		'int'    :'INT',
+		'float'  :'FLOAT',
+		'char'   :'CHAR'
+	}
+
+	tokens = [
+		'DOT','COLON','COMMA','SEMI',
+		'LPAREN','RPAREN','LBRACE','RBRACE','LBRACK','RBRACK',
+		'NE','LE','GE','EQ','LT','GT',
+		'ADD','SUB','MUL','DIV','MOD',
+		'INT_LIT','FLOAT_LIT','CHAR_LIT','STR_LIT','ID'
+	]
+
+	tokens += list(keywords.values())
+
+	t_ignore = ' \t'
+	t_ignore_COMMENT = r'[#].*'
+
+	t_DOT   = r'\.'
+	t_COLON = r':'
+	t_COMMA = r','
+	t_SEMI  = r';'
+
+	t_LPAREN = r'\('
+	t_RPAREN = r'\)'
+
+	t_LBRACE = r'\{'
+	t_RBRACE = r'\}'
+
+	t_LBRACK = r'\['
+	t_RBRACK = r'\]'
+
+	t_NE = r'<>'
+	t_LE = r'<='
+	t_GE = r'>='
+
+	t_EQ = r'='
+	t_LT = r'<'
+	t_GT = r'>'
+
+	t_ADD = r'\+'
+	t_SUB = r'-'
+
+	t_MUL = r'\*'
+	t_DIV = r'/'
+	t_MOD = r'%'
+
+	def t_CHAR_LIT(self, t):
+		r'\'([^\\\n]|(\\[abtnvfre\\\'\"])|(\\[0-3][0-7][0-7]))\''
+		value = t.value[1:-1]
+		value = ESCAPE_SEQUENCES.get(value) or int(value[1:], base=8) if '\\' in value else ord(value)
+		t.value = (value, ctypes.c_char(value))
+		return t
+
+	def t_empty_char_lit(self, t):
+		r'\'\''
+		column = t.lexer.lexpos - t.lexer.linestart - len(t.value)
+		print("Empty char literal at line", t.lineno, "column", column)
+
+	def t_long_char_lit(self, t):
+		r'\'([^\\\n]|(\\.))+\''
+		column = t.lexer.lexpos - t.lexer.linestart - len(t.value)
+		print("Invalid char literal", t.value, "at line", t.lineno, "column", column)
+
+	def t_unclosed_char_lit(self, t):
+		r'\'([^\\\n]|(\\.))*'
+		column = t.lexer.lexpos - t.lexer.linestart - len(t.value)
+		print("Unclosed char literal", t.value, "at line", t.lineno, "column", column)
+
+	def t_STR_LIT(self, t):
+		r'\"([^\\\n]|(\\[abtnvfre\\\'\"])|(\\[0-3][0-7][0-7]))*\"'
+		value = t.value[1:-1]
+		t.value = value
+		return t
+
+	def t_unclosed_str_lit(self, t):
+		r'\"([^\\\n]|(\\.))*'
+		column = t.lexer.lexpos - t.lexer.linestart - len(t.value)
+		print("Unclosed string literal", t.value, "at line", t.lineno, "column", column)
+
+	def t_FLOAT_LIT(self, t):
+		r'\d+\.\d+'
+		t.value = ctypes.c_float(float(t.value))
+		return t
+
+	def t_INT_LIT(self, t):
+		r'\d+'
+		t.value = ctypes.c_int32(int(t.value))
+		return t
+
+	def t_ID(self, t):
+		r'[a-zA-Z_][a-zA-Z_0-9]*'
+		t.type = Lexer.keywords.get(t.value,'ID')
+		return t
+
+	def t_newline(self, t):
+		r'\n'
+		t.lexer.lineno += 1
+		t.lexer.linestart = t.lexer.lexpos
+
+	def t_error(self, t):
+		err_val  = t.value[0]
+		err_line = t.lexer.lineno
+		err_col  = t.lexer.lexpos - t.lexer.linestart - len(err_val)
+		print("Illegal character '%s'" % err_val, "at line", err_line, "column", err_col)
+		t.lexer.skip(1)
+
+	def build(self, **kwargs):
+		self.lexer = lex.lex(module=self, **kwargs)
+		self.lexer.linestart = self.lexer.lexpos
+
+	def lex(self, input):
+		self.lexer.input(input)
+		for token in self.lexer:
+			yield token
+
+	def __init__(self):
+		self.build()
+
+if __name__ == '__main__':
+	lexer = Lexer()
+	with open("examples\\incorrect\\char_str.txt", 'r', encoding='utf-8') as f:
+		code = f.read()
+		for t in lexer.lex(code):
+			print(t.type, "val=", t.value, " at line ", t.lineno)
